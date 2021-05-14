7509ecfd757ebc20b5b4c5bf4a4480013bb8c5cd
Velimudr <velimudr@yandex.ru>
2021-05-14 12:11:58 +0400

    Д/З к уроку 2.

--- less2hw/blog/app.py
+++ less2hw/blog/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, render_template
 from blog.views.users import users_bp
+from blog.views.articles import articles_bp
 
 blog_app = Flask(__name__)
 
 blog_app.register_blueprint(users_bp, url_prefix='/users')
+blog_app.register_blueprint(articles_bp, url_prefix='/articles')
 
 @blog_app.route('/')
 @blog_app.route('/<name>')
--- less2hw/blog/views/users.py
+++ less2hw/blog/views/users.py
@@ -1,22 +1,20 @@
 from flask import Blueprint, render_template
+from json import load
 from werkzeug.exceptions import NotFound
 
 users_bp = Blueprint('users_bp', __name__)
 
-USERS = {
-    1:'Маша',
-    2:'Миша',
-    3:'Паша'
-}
+def load_users():
+    with open('db/users.json', 'r', encoding='utf-8') as     f:
+        return load(f)
+
+ujs = load_users()
 
 @users_bp.route('/', endpoint='list')
 def users_list():
-    return render_template('users/list.html', users=USERS)
+    return render_template('users/list.html', users=ujs)
 
 @users_bp.route('/<int:user_id>/', endpoint='details')
 def user_details(user_id: int):
-    try:
-        user_name = USERS[user_id]
-    except KeyError:
-        raise NotFound(f'Пользователь #{user_id} Не существует.')
-    return render_template('users/details.html', user_name=user_name, user_id=user_id)
+    user = [u for u in ujs if u['id'] == int(user_id)][0]
+    return render_template('users/details.html', user=user)
